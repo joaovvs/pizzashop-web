880a938cf8805b5ddb00c65f737947868d208dd0
Joao Vinicius <joaovvs@gmail.com>
2024-02-15 15:49:20 -0300

    first commit

---
+++ src/pages/_layouts/auth.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+
+export function AuthLayout() {
+  return (
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col border-r border-foreground/5 bg-muted p-10 text-muted-foreground"></div>
+      <h1>Autenticação</h1>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
